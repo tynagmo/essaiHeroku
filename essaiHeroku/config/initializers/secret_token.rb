f4ca8c2bcf6bd9f2535e07792af497574a33b2be
Quentin Maginot <quentin.maginot@gmail.com>
2012-12-05 14:57:45 +0100

    initial commit

---
+++ essaiHeroku/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EssaiHeroku::Application.config.secret_token = '15f75378a421e47f8e502b8d3aa2066bef03c3eef7aef1a5b525e412dad3061e44f9e5f71f6bb7c31ad25924da7b482fb6ae6864ea772ce057278f39999ea44e'
